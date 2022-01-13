ee75b5d3831087176932be8ee128d15cfda0354c
vignesh <96234695+Wvignesh@users.noreply.github.com>
2022-01-13 14:14:14 +0530

    imageshow

---
+++ domimg/index.js
@@ -0,0 +1,37 @@
+const img1 = document.createElement("img");
+img1.setAttribute("src","https://wallpaperaccess.com/full/2703791.jpg");
+img1.setAttribute("class","first");
+document.body.append(img1);
+
+
+const img2 = document.createElement("img");
+img2.setAttribute("src", "https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg");
+img2.setAttribute("class","second");
+document.body.append(img2);
+
+
+const img3 = document.createElement("img");
+img3.setAttribute("src", "https://www.teahub.io/photos/full/1-12815_purplish-blue-landscape-wallpaper-purple-and-blue-landscape.jpg");
+img3.setAttribute("class", "third");
+document.body.append(img3);
+
+
+const img4 = document.createElement("img");
+img4.setAttribute("src","https://mocah.org/uploads/posts/343911-Neon-Scenery-Landscape-Digital-Art.jpg");
+img4.setAttribute("class", "fourth");
+document.body.append(img4);
+
+
+const img5 = document.createElement("img");
+img5.setAttribute("src","https://cdn.theatlantic.com/thumbor/geXz--6uEjMdFqOvZq-3U_mbjHg=/1200x888/media/img/photo/2020/11/top-shots-2020-international-landsc/a01_Yuen_MagicalNight/original.jpg");
+img5.setAttribute("class","fifth");
+document.body.append(img5);
+
+
+
+const flag= document.createElement("img");
+flag.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/7/7b/India_flag_300.png");
+flag.setAttribute("class","newone");
+document.body.append(flag);
+
+  
